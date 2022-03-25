6096c7c43976a93f2ef44ffbe0bca81e4c138283
hieudx99 <hieudx1104@gmail.com>
2022-03-25 11:10:23 +0700

    add: import lombok

--- app/src/main/java/com/example/eparking/model/Customer.java
+++ app/src/main/java/com/example/eparking/model/Customer.java
@@ -1,5 +1,15 @@
 package com.example.eparking.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     private int id;
     private String fullname;
@@ -10,16 +20,6 @@ public class Customer {
     private String password;
 
 
-    public Customer(int id, String fullname, String identityCard, String telephone, String address, String username, String password) {
-        this.id = id;
-        this.fullname = fullname;
-        this.identityCard = identityCard;
-        this.telephone = telephone;
-        this.address = address;
-        this.username = username;
-        this.password = password;
-    }
-
     public Customer(String fullname, String identityCard, String telephone, String address, String username, String password) {
         this.fullname = fullname;
         this.identityCard = identityCard;
@@ -29,59 +29,4 @@ public class Customer {
         this.password = password;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullname;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullname = fullName;
-    }
-
-    public String getIdentityCard() {
-        return identityCard;
-    }
-
-    public void setIdentityCard(String identityCard) {
-        this.identityCard = identityCard;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- app/src/main/java/com/example/eparking/model/dto/Credential.java
+++ app/src/main/java/com/example/eparking/model/dto/Credential.java
@@ -1,11 +1,17 @@
 package com.example.eparking.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Credential {
     private String username;
     private String password;
 
-    public Credential(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+
 }
