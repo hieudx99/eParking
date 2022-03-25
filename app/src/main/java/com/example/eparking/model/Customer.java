473663fd7252d9e2b4cd1e3e1986f6cf4bdffb8f
hieudx99 <hieudx1104@gmail.com>
2022-03-25 11:38:14 +0700

    delete: remove lombok(android new version not suport lombok)

--- app/src/main/java/com/example/eparking/model/Customer.java
+++ app/src/main/java/com/example/eparking/model/Customer.java
@@ -6,10 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Customer {
     private int id;
     private String fullname;
@@ -19,6 +15,18 @@ public class Customer {
     private String username;
     private String password;
 
+    public Customer() {
+    }
+
+    public Customer(int id, String fullname, String identityCard, String telephone, String address, String username, String password) {
+        this.id = id;
+        this.fullname = fullname;
+        this.identityCard = identityCard;
+        this.telephone = telephone;
+        this.address = address;
+        this.username = username;
+        this.password = password;
+    }
 
     public Customer(String fullname, String identityCard, String telephone, String address, String username, String password) {
         this.fullname = fullname;
@@ -28,5 +36,62 @@ public class Customer {
         this.username = username;
         this.password = password;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getIdentityCard() {
+        return identityCard;
+    }
+
+    public void setIdentityCard(String identityCard) {
+        this.identityCard = identityCard;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 }
--- app/src/main/java/com/example/eparking/model/dto/Credential.java
+++ app/src/main/java/com/example/eparking/model/dto/Credential.java
@@ -5,13 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Credential {
     private String username;
     private String password;
 
+    public Credential() {
+    }
 
+    public Credential(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
