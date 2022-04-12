dddf6d7c2054478345edb44f433148290fdb48db
hieudx99 <hieudx1104@gmail.com>
2022-04-12 14:05:29 +0700

    user detail scroll

--- app/src/main/java/com/example/eparking/view/admin/UserDetailActivity.java
+++ app/src/main/java/com/example/eparking/view/admin/UserDetailActivity.java
@@ -1,6 +1,8 @@
 package com.example.eparking.view.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 import com.example.eparking.R;
 import com.example.eparking.model.User;
+import com.example.eparking.view.adapter.UserCarAdapter;
 
 public class UserDetailActivity extends AppCompatActivity {
 
@@ -20,6 +23,8 @@ public class UserDetailActivity extends AppCompatActivity {
     private EditText edt_identityCard;
     private EditText edt_telephone;
     private EditText edt_address;
+    private RecyclerView rcvUserCar;
+    private UserCarAdapter userCarAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +39,16 @@ public class UserDetailActivity extends AppCompatActivity {
         edt_telephone = findViewById(R.id.edt_telephone);
         edt_address = findViewById(R.id.edt_address);
 
+        rcvUserCar = findViewById(R.id.rcv_admin_user_detail_list_car);
+
         toolbar_title.setText("THÔNG TIN KHÁCH HÀNG");
         toolbar_back_icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnBackListener();
+                finish();
             }
         });
+
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra("user");
         edt_fullname.setText(user.getFullname());
@@ -48,9 +56,21 @@ public class UserDetailActivity extends AppCompatActivity {
         edt_telephone.setText(user.getTelephone());
         edt_address.setText(user.getAddress());
 
+        //init recycler view
+        LinearLayoutManager layoutManager = new LinearLayoutManager(UserDetailActivity.this,
+                LinearLayoutManager.VERTICAL, false) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+        rcvUserCar.setLayoutManager(layoutManager);
+        userCarAdapter = new UserCarAdapter();
+        userCarAdapter.setData(user.getListCar());
+        rcvUserCar.setAdapter(userCarAdapter);
+        //
+
     }
 
-    private void btnBackListener() {
-        finish();
-    }
+
 }
